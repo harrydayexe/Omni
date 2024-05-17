690832c904a33e67cc120f2e4951705f94b8604a
Harry Day <harry.day-2@student.manchester.ac.uk>
2024-05-17 23:37:33 +0100

    Fix database repo type

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -2,11 +2,13 @@
 // and access.
 package storage
 
+import "github.com/harrydayexe/Omni/internal/snowflake"
+
 // Database is an interface for accessing persisted data.
 // It provides methods for standard CRUD operations.
 //
 // Database has an associated type T which must conform to Identifier.
-type Database[T Identifier] interface {
+type Database[T snowflake.Identifier] interface {
 	// Create adds entity to the database.
 	// The returned error is nil if the operation is successful, otherwise it
 	// contains the error that occurred.
