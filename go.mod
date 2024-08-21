242e6e653fa4bac1c13441a871d140a24a2218e5
Harry Day <harry.day-2@student.manchester.ac.uk>
2024-08-21 16:49:02 +0100

    test(omniread): Add tests for GET user handler

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.22.3
 
 require (
 	github.com/go-sql-driver/mysql v1.8.1
-	github.com/testcontainers/testcontainers-go v0.33.0
 	github.com/testcontainers/testcontainers-go/modules/mysql v0.33.0
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -46,6 +45,7 @@ require (
 	github.com/shirou/gopsutil/v3 v3.23.12 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/testcontainers/testcontainers-go v0.33.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
---
+++ internal/omniread/api/routes_test.go
@@ -0,0 +1,159 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/harrydayexe/Omni/internal/models"
+	"github.com/harrydayexe/Omni/internal/snowflake"
+	"github.com/harrydayexe/Omni/internal/storage"
+)
+
+type MockUserRepo struct {
+	readFunc   func(ctx context.Context, id snowflake.Snowflake) (*models.User, error)
+	createFunc func(ctx context.Context, entity models.User) error
+	updateFunc func(ctx context.Context, entity models.User) error
+	deleteFunc func(ctx context.Context, id snowflake.Snowflake) error
+}
+
+func (m MockUserRepo) Read(ctx context.Context, id snowflake.Snowflake) (*models.User, error) {
+	return m.readFunc(ctx, id)
+}
+func (m MockUserRepo) Create(ctx context.Context, entity models.User) error {
+	return m.createFunc(ctx, entity)
+}
+func (m MockUserRepo) Update(ctx context.Context, entity models.User) error {
+	return m.updateFunc(ctx, entity)
+}
+func (m MockUserRepo) Delete(ctx context.Context, id snowflake.Snowflake) error {
+	return m.deleteFunc(ctx, id)
+}
+
+func TestGetUserKnown(t *testing.T) {
+	mockedRepo := &MockUserRepo{
+		readFunc: func(ctx context.Context, id snowflake.Snowflake) (*models.User, error) {
+			newUser := models.NewUser(id, "johndoe", make([]snowflake.Snowflake, 0))
+			return &newUser, nil
+		},
+	}
+
+	req, err := http.NewRequest("GET", "/user/1796290045997481984", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := NewHandler(
+		slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{})),
+		mockedRepo,
+		nil,
+		nil,
+	)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	if rr.Header().Get("Content-Type") != "application/json" {
+		t.Errorf("handler returned wrong content type: got %v want %v",
+			rr.Header().Get("Content-Type"), "application/json")
+	}
+
+	expected := `{"id":1796290045997481984,"username":"johndoe","posts":[]}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
+
+func TestGetUserUnknown(t *testing.T) {
+	mockedRepo := &MockUserRepo{
+		readFunc: func(ctx context.Context, id snowflake.Snowflake) (*models.User, error) {
+			return nil, nil
+		},
+	}
+
+	req, err := http.NewRequest("GET", "/user/1796290045997481984", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := NewHandler(
+		slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{})),
+		mockedRepo,
+		nil,
+		nil,
+	)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetUserBadFormedId(t *testing.T) {
+	mockedRepo := &MockUserRepo{
+		readFunc: func(ctx context.Context, id snowflake.Snowflake) (*models.User, error) {
+			return nil, nil
+		},
+	}
+
+	req, err := http.NewRequest("GET", "/user/hello", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := NewHandler(
+		slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{})),
+		mockedRepo,
+		nil,
+		nil,
+	)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
+
+func TestGetUserDBError(t *testing.T) {
+	mockedRepo := &MockUserRepo{
+		readFunc: func(ctx context.Context, id snowflake.Snowflake) (*models.User, error) {
+			return nil, storage.NewDatabaseError("database error", errors.New("database error"))
+		},
+	}
+
+	req, err := http.NewRequest("GET", "/user/1796290045997481984", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := NewHandler(
+		slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{})),
+		mockedRepo,
+		nil,
+		nil,
+	)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+}
