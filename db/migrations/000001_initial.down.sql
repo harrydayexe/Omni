23c72b561dff45abbc2dff48e1477d36bc0fe7aa
Harry Day <harry.day-2@student.manchester.ac.uk>
2024-10-15 00:11:55 +0100

    feat(db): Add docker compose for db
    This also includes using golang migrate to perform database migrations

---
+++ db/migrations/000001_initial.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE Users;
+DROP TABLE Posts;
+DROP TABLE Comments;
+DROP INDEX idx_user_id;
+DROP INDEX idx_post_id;
--- db/migrations/000001_initial.up.sql
+++ db/migrations/000001_initial.up.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Users 
+CREATE TABLE IF NOT EXISTS Users 
 (
     id BIGINT NOT NULL,
     username VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE Posts
+CREATE TABLE IF NOT EXISTS Posts
 (
     id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE Posts
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Comments (
+CREATE TABLE IF NOT EXISTS Comments (
     id BIGINT NOT NULL,
     post_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
@@ -27,3 +27,6 @@ CREATE TABLE Comments (
     FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
     FOREIGN KEY (post_id) REFERENCES Posts(id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_user_id ON Posts(user_id);
+CREATE INDEX idx_post_id ON Comments(post_id);
