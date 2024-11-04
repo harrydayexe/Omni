8bab193eb327bcc907ee448e1c73d53640fb51bd
Harry Day <harry.day-2@student.manchester.ac.uk>
2024-11-04 22:16:27 +0000

    test(lb): Finish test for round robin

--- internal/loadbalancer/balancer/round_robin_test.go
+++ internal/loadbalancer/balancer/round_robin_test.go
@@ -3,6 +3,7 @@ package balancer
 import (
 	"net/url"
 	"reflect"
+	"sync/atomic"
 	"testing"
 )
 
@@ -201,6 +202,95 @@ func TestRoundRobinRemove(t *testing.T) {
 			}
 			if !reflect.DeepEqual(c.bal, c.want) {
 				t.Errorf("Test: %s failed\n got: %v,\nwant: %v", c.name, c.bal, c.want)
+			}
+		})
+	}
+}
+
+func TestRoundRobinBalance(t *testing.T) {
+	type expected struct {
+		isErrorExpected bool
+		want            url.URL
+		error           error
+	}
+	cases := []struct {
+		name string
+		bal  Balancer
+		reqs []expected
+	}{
+		{
+			name: "Balance with no URL",
+			bal: &RoundRobinBalancer{
+				BaseBalancer: BaseBalancer{
+					servers: []url.URL{},
+				},
+			},
+			reqs: []expected{
+				{
+					isErrorExpected: true,
+					want:            url.URL{},
+					error:           NoHealthyHostsError,
+				},
+			},
+		},
+		{
+			name: "Balance with one URL",
+			bal: &RoundRobinBalancer{
+				BaseBalancer: BaseBalancer{
+					servers: []url.URL{
+						{Scheme: "http", Host: "localhost:8080"},
+					},
+				},
+			},
+			reqs: []expected{
+				{
+					isErrorExpected: false,
+					want:            url.URL{Scheme: "http", Host: "localhost:8080"},
+					error:           nil,
+				},
+			},
+		},
+		{
+			name: "Balance with two URLs",
+			bal: &RoundRobinBalancer{
+				BaseBalancer: BaseBalancer{
+					servers: []url.URL{
+						{Scheme: "http", Host: "localhost:4040"},
+						{Scheme: "http", Host: "localhost:8080"},
+					},
+				},
+				current: atomic.Uint64{},
+			},
+			reqs: []expected{
+				{
+					isErrorExpected: false,
+					want:            url.URL{Scheme: "http", Host: "localhost:8080"},
+					error:           nil,
+				},
+				{
+					isErrorExpected: false,
+					want:            url.URL{Scheme: "http", Host: "localhost:4040"},
+					error:           nil,
+				},
+				{
+					isErrorExpected: false,
+					want:            url.URL{Scheme: "http", Host: "localhost:8080"},
+					error:           nil,
+				},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			for _, req := range c.reqs {
+				got, err := c.bal.Balance()
+				if req.isErrorExpected && err != req.error {
+					t.Errorf("Test: %s failed\n got error: %v\nwant error: %v", c.name, err, req.error)
+				}
+				if !req.isErrorExpected && !reflect.DeepEqual(got, req.want) {
+					t.Errorf("Test: %s failed\n got: %v,\nwant: %v", c.name, got, req.want)
+				}
 			}
 		})
 	}
