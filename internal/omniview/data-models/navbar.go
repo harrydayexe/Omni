5b446b462d8e0da8a3d05c878fbd93a7356096a7
Harry Day <contact@harryday.xyz>
2025-03-10 20:01:18 +0000

    feat(OmniView): Add profile button to drop down

--- internal/omniview/data-models/navbar.go
+++ internal/omniview/data-models/navbar.go
@@ -7,11 +7,18 @@ type NavBar struct {
 	ShouldShowLogin bool
 	// IsLoggedIn is a boolean that determines if the user is logged in
 	IsLoggedIn bool
+	// ID is the ID of the user that is logged in
+	ID uint64
 }
 
 func NewNavBar(ctx context.Context) NavBar {
+	var userID uint64
+	if ctx.Value("user-id") != nil {
+		userID = ctx.Value("user-id").(uint64)
+	}
 	return NavBar{
 		ShouldShowLogin: true,
 		IsLoggedIn:      ctx.Value("is-logged-in").(bool),
+		ID:              userID,
 	}
 }
