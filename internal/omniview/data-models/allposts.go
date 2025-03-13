275deb8311dce7d5d50fcd5838a283550dbaf306
Harry Day <contact@harryday.xyz>
2025-03-13 16:34:33 +0000

    feat(OmniView): Show delete button on posts in list

--- internal/omniview/data-models/allposts.go
+++ internal/omniview/data-models/allposts.go
@@ -2,7 +2,14 @@ package datamodels
 
 import (
 	"github.com/harrydayexe/Omni/internal/omniread/datamodels"
+	"github.com/harrydayexe/Omni/internal/snowflake"
+	"github.com/harrydayexe/Omni/internal/utilities"
 )
+
+type PostListItem struct {
+	datamodels.PostAndUsername
+	IsDeleteable bool
+}
 
 // AllPosts contains the data to drive the "posts" template
 type AllPosts struct {
@@ -11,7 +18,7 @@ type AllPosts struct {
 	// Defines if the page is for all posts or just one user's posts
 	IsUserPage bool
 	// The collection of posts to render
-	Posts []datamodels.PostAndUsername
+	Posts []PostListItem
 	// Determines whether to show the previous page button
 	HasPrevious bool
 	// Determines whether to show the next page button
@@ -27,6 +34,7 @@ type AllPosts struct {
 // and nextNum is set. If positive, the bools are true, otherwise they are false
 func NewAllPosts(
 	error string,
+	userID snowflake.Identifier,
 	posts datamodels.AllPosts,
 	isUserPage bool,
 	prevNum,
@@ -42,14 +50,34 @@ func NewAllPosts(
 		hasNext = true
 		nextNumP = nextNum
 	}
+	postsList := utilities.Map(posts.Posts, convertPostAndUsernameToPostListItem(userID))
 
 	return AllPosts{
 		Error:              error,
-		Posts:              posts.Posts,
+		Posts:              postsList,
 		IsUserPage:         isUserPage,
 		HasPrevious:        hasPrev,
 		HasNext:            hasNext,
 		PreviousPageNumber: prevNumP,
 		NextPageNumber:     nextNumP,
+	}
+}
+
+func convertPostAndUsernameToPostListItem(
+	userID snowflake.Identifier,
+) func(datamodels.PostAndUsername) PostListItem {
+	if userID == nil {
+		return func(p datamodels.PostAndUsername) PostListItem {
+			return PostListItem{
+				PostAndUsername: p,
+				IsDeleteable:    false,
+			}
+		}
+	}
+	return func(p datamodels.PostAndUsername) PostListItem {
+		return PostListItem{
+			PostAndUsername: p,
+			IsDeleteable:    p.Post.UserID == int64(userID.Id().ToInt()),
+		}
 	}
 }
