c753077aa4550b414a730f0b8c8adb933e58f313
Harry Day <contact@harryday.xyz>
2025-04-24 15:14:41 +0100

    fix(diss): Rename code snippet files for go test

--- dissertation/code-snippets/go-env-example.go
+++
@@ -1,9 +0,0 @@
-func main() {
-	ctx := context.Background()
-	cfg, err := env.ParseAs[config.DatabaseConfig]()
-	if err != nil {
-		panic(err)
-	}
-
-	// Start-Up Code...
-}
--- dissertation/code-snippets/sqlc-go-models.go
+++
@@ -1,8 +0,0 @@
-type Post struct {
-	ID          int64     `json:"id"`
-	UserID      int64     `json:"user_id"`
-	CreatedAt   time.Time `json:"created_at"`
-	Title       string    `json:"title"`
-	MarkdownUrl string    `json:"markdown_url"`
-	Description string    `json:"description"`
-}
